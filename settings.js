95efd91e97e86268e5f205cf5442d455e561c05f
root <lpellink@gmail.com>
2023-05-09 21:26:24 -0500

    xx

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://pyonpyonkun.github.io/MXPR/',
+  repoPath: 'https://pyonpyonkun.github.io/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
